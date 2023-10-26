4d941625a421e47c1b48b11d838b577d14518674
Sardor <ismoil@gmail.com>
2023-10-26 18:46:22 +0500

    Lavash

---
+++ script.js
@@ -0,0 +1,19 @@
+const productList = {
+ 'Hot-dog': {
+  info: 'average ',
+  price: 22000
+},
+'Lavash': {
+  info: 'big',
+  price: 25000
+},
+'Fanta': {
+  info: '1,5L',
+  price: 12000
+}   
+}
+
+console.log(`Sizning buyurtmangiz ${productList}
+||yetkazib berish xizmati ${Math.round(productList + 9000)}`);
+
+ 
